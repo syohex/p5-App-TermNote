7d4de7f8eca6a7e01bed7a50de9f31c4db4b86e1
Syohei YOSHIDA <syohex@gmail.com>
2013-07-10 11:11:42 +0900

    init repos

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::TermNote - It's new $module
+
+# SYNOPSIS
+
+    use App::TermNote;
+
+# DESCRIPTION
+
+App::TermNote is ...
+
+# LICENSE
+
+Copyright (C) Syohei YOSHIDA.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Syohei YOSHIDA <syohex@gmail.com>
---
+++ lib/App/TermNote.pm
@@ -0,0 +1,39 @@
+package App::TermNote;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::TermNote - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::TermNote;
+
+=head1 DESCRIPTION
+
+App::TermNote is ...
+
+=head1 LICENSE
+
+Copyright (C) Syohei YOSHIDA.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Syohei YOSHIDA E<lt>syohex@gmail.comE<gt>
+
+=cut
+
